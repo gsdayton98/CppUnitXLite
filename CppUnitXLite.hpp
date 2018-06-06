f920e96cea397735735e8fa53b1ef6a81e8bf965
Glen Dayton <dayton@ieee.org>
2018-06-05 22:31:16 -0700

    Correct typos in README

--- CppUnitXLite.hpp
+++ CppUnitXLite.hpp
@@ -1,5 +1,5 @@
 // -*- mode:C++; c-basic-offset:2; indent-tabs-mode:nil -*-
-/* 
+/*
 Copyright (c) 2015 Glen S. Dayton
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,7 +18,7 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE. 
+THE SOFTWARE.
 */
 /**
   *  Extra light framework for unit testing in C++.
